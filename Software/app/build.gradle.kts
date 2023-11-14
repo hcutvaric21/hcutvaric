f8f39c9c6287e666a1c48da53eea7edbc672fde3
LAPTOP-HJ5CIGLB\hcutv <hcutvaric@gmail.com>
2023-11-14 14:48:44 +0100

    Fix SDK bug The application does not want to start in the emulator, so it is necessary to change the compileSdk and targetSdk versions from 33 to 34.

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "hr.foi.rampu.testingexamplesapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hr.foi.rampu.testingexamplesapplication"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
